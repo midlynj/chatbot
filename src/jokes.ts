5a24e440b0a5a14ce8212617048e7c40f5f70a4f
midlynj <jmidlyn@gmail.com>
2023-02-22 20:47:50 -0600

    jokes array created

---
+++ src/jokes.ts
@@ -0,0 +1,8 @@
+export const jokes : string[] = [
+    "What does the clock do when it's hungry?\n" +
+    "It goes back four seconds.", "A pair of cows were talking in the field. One says, “Have you heard about the mad cow disease that’s going around?”\n" +
+    "\n" +
+    "“Yeah,” the other cow says. “Makes me glad I’m a penguin.”", "Why can’t you hear a pterodactyl going to the bathroom?\n" +
+    "\n" +
+    "Because the “P” is silent"
+];
